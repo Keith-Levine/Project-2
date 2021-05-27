6d1e60c85123d8fc91217cebd6d4cbdefd3619ce
Keith Levine <Levine.Keith@gmail.com>
2021-05-27 16:35:58 -0700

    Show route created and displaying recipe information.

--- controllers/reciperoutes.js
+++ controllers/reciperoutes.js
@@ -23,4 +23,12 @@ router.post('/', (req, res)=>{
     });
 });
 
+router.get('/:id', (req, res)=>{
+    Recipe.findById(req.params.id, (err, foundRecipe)=>{
+        res.render('show.ejs', {
+            recipe:foundRecipe
+        });
+    });
+});
+
 module.exports = router;
