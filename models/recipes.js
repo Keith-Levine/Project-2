e9944bd7b953175f967e7d7f943a101f445bf517
Keith Levine <Levine.Keith@gmail.com>
2021-05-28 15:02:02 -0700

    adding tags for one to mnany relationship

--- models/recipes.js
+++ models/recipes.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     name:  { type: String, required: true },
     img: String,
+    tag: String,
     ingredients:  { type: String, required: true },
     instructions:  { type: String, required: true }
 }, { timestamps: true });
