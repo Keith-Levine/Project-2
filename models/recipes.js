35b6649e77c74e82e84417ece17525e5d9873329
Keith Levine <Levine.Keith@gmail.com>
2021-05-30 10:57:46 -0700

    Still having ossues with one to many, but getting closer"

--- models/recipes.js
+++ models/recipes.js
@@ -3,11 +3,24 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
     name:  { type: String, required: true },
     img: String,
-    tags: [{}, {}, {}],
+    tags: [{
+        type:  mongoose.Schema.Types.ObjectId,
+        ref: "Tag"
+    }],
     ingredients:  { type: String, required: true },
     instructions:  { type: String, required: true }
 }, { timestamps: true });
 
+
+const tagSchema = new mongoose.Schema({
+    info: String,
+    tags: {
+        type: String,
+    },
+});
+
 const Recipe = mongoose.model('Recipe', recipeSchema);
+const Tag = mongoose.model('Tag', tagSchema);
 
 module.exports = Recipe;
+module.exports = Tag;
